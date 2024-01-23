e4a3d64c9c51b4571bf03d5e929846fbd8ac3416
jaruwat420 <jaruwatcj@gmail.com>
2024-01-23 14:54:24 +0700

    update tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  // plugins: [require("daisyui")],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake" ,  "lemonade",],
+  },
+}
